75864bf79f229464a12385155bd8f5b8307277a1
min <103273791@qq.com>
2019-10-17 21:52:52 -0700

    Module1 newest version

--- MinQiu/Module1/prime.py
+++ MinQiu/Module1/prime.py
@@ -1,6 +1,7 @@
 # Python program to check if the input number is prime or not
 
 num = 407
+count = 0
 
 # take input from the user
 # num = int(input("Enter a number: "))
@@ -8,11 +9,14 @@ num = 407
 # prime numbers are greater than 1
 if num > 1:
    # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
+    for i in range(2,num):
+        if (num % i) == 0:
            print(i,"times",num//i,"is",num)
-   else:
-       print(num,"is a prime number")
+           count+=1
+    if count > 0 :
+        print(num,"is not a prime number")
+    else:
+        print(num,"is a prime number")
        
 # if input number is less than
 # or equal to 1, it is not prime
